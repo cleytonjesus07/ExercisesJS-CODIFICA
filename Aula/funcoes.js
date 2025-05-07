6d01a300532f61d05f4146ee656c7d3c32ecbb60
Cleyton Jesus <cleytonjesus007@gmail.com>
2025-05-06 23:10:28 -0300

    funções

---
+++ Aula/funcoes.js
@@ -0,0 +1,37 @@
+/* Funções */
+
+/* 2 tipos:
+
+- As que retornam valores
+- As que não retornam: chamadas de procedimento.
+
+*/
+
+/* function RetornaParOuImpar(num) {
+    return num % 2 === 0 ? "Par" : "Ímpar";
+}
+
+console.log(RetornaParOuImpar(2)) */
+
+/* function calcularArea(altura, largura) {
+    return altura * largura;
+}
+
+console.log(calcularArea(10, 10)) */
+/* 
+function ordenarArray(arr) {
+    return arr.sort((a, b) => a - b);
+}
+let numsArr = [5, 1, -1, 0, 4, 5];
+console.log(ordenarArray(numsArr));
+ */
+
+/* function somarArrayElementos(arr) {
+    return arr.reduce((a, b) => a + b, 0);
+}
+function maiorNumeroArray(arr) {
+    return Math.max(...arr);
+} */
+
+console.log(maiorNumeroArray([10, 2, 300]))
+
